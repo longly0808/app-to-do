8cbf9192c1d14dba612f5e4a1a65903f8ce3c4d2
gnollys88 <longlb@nutifood.com.vn>
2022-10-26 13:17:20 +0700

    fix overflow

--- lib/page/auth/login_page.dart
+++ lib/page/auth/login_page.dart
@@ -40,35 +40,36 @@ class _LoginPageState
             width: size.width,
             child:Form(
               key:_formKey ,
-              child:  Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: size.width,
-                    alignment: Alignment.bottomLeft,
-                    padding: const EdgeInsets.all(
-                      Dimens.size16,
-                    ),
-                    child: InkWell(
-                      onTap: () {},
-                      child: const Icon(
-                        Icons.arrow_back_ios,
-                        color: MyColors.blackColor,
-                        size: Dimens.size20,
+              child:  SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: size.width,
+                      alignment: Alignment.bottomLeft,
+                      padding: const EdgeInsets.all(
+                        Dimens.size16,
+                      ),
+                      child: InkWell(
+                        onTap: () {},
+                        child: const Icon(
+                          Icons.arrow_back_ios,
+                          color: MyColors.blackColor,
+                          size: Dimens.size20,
+                        ),
                       ),
                     ),
-                  ),
-                  // Text(tr('to_do_list'), style: theme.textTheme.headline1,)
-                  Padding(
-                    padding: const EdgeInsets.all(Dimens.size16),
-                    child: Image.asset(
-                      Constants.logo_intro,
-                      fit: BoxFit.cover,
-                      color: theme.colorScheme.primary,
-                      height: size.height / 3,
+                    // Text(tr('to_do_list'), style: theme.textTheme.headline1,)
+                    Padding(
+                      padding: const EdgeInsets.all(Dimens.size16),
+                      child: Image.asset(
+                        Constants.logo_intro,
+                        fit: BoxFit.cover,
+                        color: theme.colorScheme.primary,
+                        height: size.height / 3,
+                      ),
                     ),
-                  ),
-                  Padding(
+                    Padding(
                       padding: const EdgeInsets.all(Dimens.size16),
                       child: CustomTextField(
                         isTypePassword: false,
@@ -77,72 +78,73 @@ class _LoginPageState
                           _email = value;
                         },
                       ),),
-                  Padding(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: Dimens.size16),
-                    child: CustomTextField(
-                      isTypePassword: true,
-                      hintText: tr('your_password'),
-                      onChange: (value){
-                        _password = value;
-                      },
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(
-                      Dimens.size16,
-                    ),
-                    width: size.width,
-                    alignment: Alignment.bottomRight,
-                    child: InkWell(
-                      onTap: () {},
-                      child: Text(
-                        tr('forgot_password'),
-                        style: theme.textTheme.headline5,
+                    Padding(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: Dimens.size16),
+                      child: CustomTextField(
+                        isTypePassword: true,
+                        hintText: tr('your_password'),
+                        onChange: (value){
+                          _password = value;
+                        },
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(Dimens.size16),
-                    child: CustomButton(
-                      textColors: theme.colorScheme.onSecondary,
-                      onclick: () {
-                        // var service = AppDependencies.injector.get<AuthService>();
-                        // service.LoginAuthentication();
-                        if(   _formKey.currentState!.validate()){
-                          bloc.checkLogin(email: _email ?? '' ,password: _password ?? '' );
-                        }
-                      },
-                      text: tr('login'),
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: Dimens.size16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          tr('text_signup'),
+                    Container(
+                      padding: const EdgeInsets.all(
+                        Dimens.size16,
+                      ),
+                      width: size.width,
+                      alignment: Alignment.bottomRight,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          tr('forgot_password'),
                           style: theme.textTheme.headline5,
                         ),
-                        const SizedBox(width: Dimens.size8),
-                        Flexible(
-                          child: InkWell(
-                            onTap: () {
-                            },
-                            child: Text(
-                              tr('signup'),
-                              style: theme.textTheme.headline5!
-                                  .copyWith(color: theme.colorScheme.error),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(Dimens.size16),
+                      child: CustomButton(
+                        textColors: theme.colorScheme.onSecondary,
+                        onclick: () {
+                          // var service = AppDependencies.injector.get<AuthService>();
+                          // service.LoginAuthentication();
+                          if(   _formKey.currentState!.validate()){
+                            bloc.checkLogin(email: _email ?? '' ,password: _password ?? '' );
+                          }
+                        },
+                        text: tr('login'),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: Dimens.size16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            tr('text_signup'),
+                            style: theme.textTheme.headline5,
+                          ),
+                          const SizedBox(width: Dimens.size8),
+                          Flexible(
+                            child: InkWell(
+                              onTap: () {
+                              },
+                              child: Text(
+                                tr('signup'),
+                                style: theme.textTheme.headline5!
+                                    .copyWith(color: theme.colorScheme.error),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              )
             )
           );
         },
